1079c94f818069ac88c2016ef8ae2755c33ca975
Ruijie Zhang <r82zhang@edu.uwaterloo.ca>
2019-12-29 22:14:56 -0500

    question 4 correct file

--- 4  Ugly Number II.py
+++ 4  Ugly Number II.py
@@ -18,6 +18,7 @@ Ugly number is a number that only have prime factors 2, 3 and 5.
 
 
 """***************************  CODE  ****************************"""
+import heapq
 class Solution:
     """
     @param n: An integer
@@ -25,4 +26,23 @@ class Solution:
     """
     def nthUglyNumber(self, n):
         # write your code here
-
+        # using bfs
+        
+        q = []
+        count = 1
+        q.append(1)
+        
+        visited =set()
+        while q:
+            p = heapq.heappop(q)
+            if count == n:
+                return p
+            else:
+                count += 1
+                p1 = p *2
+                p2 = p *3
+                p3 = p *5
+                for e in [p1, p2, p3]:
+                    if e not in visited:
+                        visited.add(e)
+                        heapq.heappush(q, e)
