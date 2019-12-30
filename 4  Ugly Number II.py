ee62e494f44d0924500d22fcbd9b454bdf8bb23f
Ruijie Zhang <r82zhang@edu.uwaterloo.ca>
2019-12-29 22:12:30 -0500

    question 4

---
+++ 4  Ugly Number II.py
@@ -0,0 +1,28 @@
+"""***************************  TITLE  ****************************"""
+"""4  Ugly Number II.py"""
+
+
+
+"""***************************  DESCRIPTION  ****************************"""
+"""
+Ugly number is a number that only have prime factors 2, 3 and 5.
+"""
+
+
+
+"""***************************  EXAMPLES  ****************************"""
+"""
+2
+"""
+
+
+
+"""***************************  CODE  ****************************"""
+class Solution:
+    """
+    @param n: An integer
+    @return: return a  integer as description.
+    """
+    def nthUglyNumber(self, n):
+        # write your code here
+
