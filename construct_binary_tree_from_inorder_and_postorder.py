10e660959039680dc9941a98b487dbcf26ddaa78
Ruijie <r82zhang@edu.uwaterloo.ca>
2020-03-10 14:04:54 -0400

    doing binary tree related questions

--- construct_binary_tree_from_inorder_and_postorder.py
+++ construct_binary_tree_from_inorder_and_postorder.py
@@ -6,19 +6,19 @@ class TreeNode:
         self.right = None
 
 class Solution:
-    def buildTree(self, postorder: List[int], inorder: List[int]) -> TreeNode:
+    def buildTree(self, postorder, inorder):
         if not postorder or not inorder:
             return None
         
-        root = TreeNode(postorder[0])
+        root = TreeNode(postorder[-1])
         if len(postorder) == 1:
             return root
         
         else:
             root_idx = inorder.index(root.val)
             left_tree_len = root_idx
-            root.left = self.buildTree(postorder[0: left_tree_len+1], inorder[:root_idx])
-            root.right = self.buildTree(postorder[left_tree_len+1:-1], inorder[root_idx+1:])
+            root.left = self.buildTree(postorder[0: left_tree_len], inorder[:root_idx])
+            root.right = self.buildTree(postorder[left_tree_len:-1], inorder[root_idx+1:])
 
             return root
 
@@ -72,7 +72,28 @@ def postorder_traversal(root):
     return res[::-1]
 
 
+def compareTwoTree(t1, t2):
+    if t1 is None and t2 is None:
+        return True
+
+    if not t1 or not t2:
+        return False
+
+    return t1.val == t2.val and compareTwoTree(t1.left, t2.left) and compareTwoTree(t1.right, t2.right)
 
 
 
 if __name__ == '__main__':
+    root = TreeNode(1)
+    root.left = TreeNode(2)
+    root.right = TreeNode(3)
+    root.left.left = TreeNode(4)
+    root.left.right = TreeNode(5)
+    root.right.left = TreeNode(6)
+    root.right.right = TreeNode(7)
+
+    soln = Solution()
+    postorder, inorder = postorder_traversal(root), inorder_traversal(root)
+    new_tree = soln.buildTree(postorder, inorder)
+    print(compareTwoTree(root, new_tree))
+
